2eb32d6e2125f41c7a7f5a505399db767637d8db
Song Yangyu <flyfy1@gmail.com>
2023-12-03 20:54:27 +0800

    Remove the useless dbsetup code

--- cmds/migrate/main.go
+++ cmds/migrate/main.go
@@ -8,10 +8,11 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/todopeer/backend/orm"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "diarier.db")
+	db, err := sql.Open("sqlite3", orm.DBPATH)
 	if err != nil {
 		log.Fatalf("Could not connect to db: %v\n", err)
 	}
--- orm/init.go
+++ orm/init.go
@@ -1,20 +1,3 @@
 package orm
 
-import (
-	"database/sql"
-	"log"
-)
-
 const DBPATH = "diarier.db"
-
-func GetDB() *sql.DB {
-	db, err := sql.Open("sqlite3", DBPATH)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if db == nil {
-		log.Fatal("db nil")
-	}
-	return db
-}
